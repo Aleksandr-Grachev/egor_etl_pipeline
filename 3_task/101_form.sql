f88773299445e8390d33854b4664e2828c8812a0
bokla <boklagov.egor@yandex.ru>
2025-01-09 10:49:37 +0300

    v.5

---
+++ 3_task/101_form.sql
@@ -0,0 +1,162 @@
+CREATE OR REPLACE PROCEDURE "DM".fill_f101_round_f(
+    i_ondate date)
+LANGUAGE 'plpgsql'
+AS $BODY$
+DECLARE
+	v_start_time_log timestamp;
+    v_end_time_log timestamp;
+	v_duration_log interval;
+
+    start_date DATE := i_OnDate - INTERVAL '1 month';
+    end_date DATE := i_OnDate - INTERVAL '1 day';
+BEGIN
+
+	v_start_time_log := clock_timestamp();
+	PERFORM pg_sleep(5);
+	
+    -- Логи
+    INSERT INTO "LOGS".ETL_LOG (PROCESS_NAME, LOG_DATE, START_TIME, END_TIME, STATUS, DURATION)
+    VALUES ('fill_f101_round_f', CURRENT_DATE, v_start_time_log, NULL, 'Start', NULL);
+	
+    -- Удаляем старые данные
+    DELETE FROM "DM".dm_f101_round_f
+    WHERE from_date = start_date AND to_date = end_date;
+
+    --Нахождение данных для вставки
+    WITH
+    account_data AS (
+        SELECT 
+            a.account_rk,
+            a.currency_code::integer,
+            a.char_type AS characteristic,
+            la.chapter,
+            la.ledger_account
+        FROM "DS".md_account_d a
+        JOIN "DS".md_ledger_account_s la
+            ON LEFT(a.account_number, 5) = la.ledger_account::varchar
+    ),
+    balance_data AS (
+        SELECT 
+            ad.chapter,
+            ad.ledger_account,
+            ad.characteristic,
+            SUM(CASE 
+                    WHEN ad.currency_code IN (810, 643) AND ab.on_date = start_date - INTERVAL '1 day' 
+                    THEN ab.balance_out_rub 
+                    ELSE 0 
+                END) AS balance_in_rub,
+            SUM(CASE 
+                    WHEN ad.currency_code NOT IN (810, 643) AND ab.on_date = start_date - INTERVAL '1 day' 
+                    THEN ab.balance_out_rub 
+                    ELSE 0 
+                END) AS balance_in_val,
+            SUM(CASE 
+                    WHEN ab.on_date = start_date - INTERVAL '1 day' 
+                    THEN ab.balance_out_rub 
+                    ELSE 0 
+                END) AS balance_in_total,
+            SUM(CASE 
+                    WHEN ad.currency_code IN (810, 643) AND ab.on_date = end_date 
+                    THEN ab.balance_out_rub 
+                    ELSE 0 
+                END) AS balance_out_rub,
+            SUM(CASE 
+                    WHEN ad.currency_code NOT IN (810, 643) AND ab.on_date = end_date 
+                    THEN ab.balance_out_rub 
+                    ELSE 0 
+                END) AS balance_out_val,
+            SUM(CASE 
+                    WHEN ab.on_date = end_date 
+                    THEN ab.balance_out_rub 
+                    ELSE 0 
+                END) AS balance_out_total
+        FROM "DM".dm_account_balance_f ab
+        JOIN account_data ad
+            ON ab.account_rk = ad.account_rk
+        GROUP BY ad.chapter, ad.ledger_account, ad.characteristic
+    ),
+    turnover_data AS (
+        SELECT 
+            ad.chapter,
+            ad.ledger_account,
+            ad.characteristic,
+            SUM(CASE 
+                    WHEN ad.currency_code IN (810, 643) AND ac.on_date BETWEEN start_date AND end_date 
+                    THEN ac.debet_amount_rub 
+                    ELSE 0 
+                END) AS turn_deb_rub,
+            SUM(CASE 
+                    WHEN ad.currency_code NOT IN (810, 643) AND ac.on_date BETWEEN start_date AND end_date 
+                    THEN ac.debet_amount_rub 
+                    ELSE 0 
+                END) AS turn_deb_val,
+            SUM(CASE 
+                    WHEN ac.on_date BETWEEN start_date AND end_date 
+                    THEN ac.debet_amount_rub 
+                    ELSE 0 
+                END) AS turn_deb_total,
+            SUM(CASE 
+                    WHEN ad.currency_code IN (810, 643) AND ac.on_date BETWEEN start_date AND end_date 
+                    THEN ac.credit_amount_rub 
+                    ELSE 0 
+                END) AS turn_cre_rub,
+            SUM(CASE 
+                    WHEN ad.currency_code NOT IN (810, 643) AND ac.on_date BETWEEN start_date AND end_date 
+                    THEN ac.credit_amount_rub 
+                    ELSE 0 
+                END) AS turn_cre_val,
+            SUM(CASE 
+                    WHEN ac.on_date BETWEEN start_date AND end_date 
+                    THEN ac.credit_amount_rub 
+                    ELSE 0 
+                END) AS turn_cre_total
+        FROM "DM".dm_account_turnover_f ac
+        LEFT JOIN account_data ad
+            ON ac.account_rk = ad.account_rk
+        GROUP BY ad.chapter, ad.ledger_account, ad.characteristic
+    )
+
+    -- Вставка
+    INSERT INTO "DM".dm_f101_round_f(
+        from_date, to_date, chapter, ledger_account, characteristic, 
+        balance_in_rub, balance_in_val, balance_in_total, 
+        turn_deb_rub, turn_deb_val, turn_deb_total, 
+        turn_cre_rub, turn_cre_val, turn_cre_total, 
+        balance_out_rub, balance_out_val, balance_out_total
+    )
+    SELECT 
+        start_date AS from_date,
+        end_date AS to_date,
+        bd.chapter,
+        bd.ledger_account,
+        bd.characteristic,
+        bd.balance_in_rub,
+        bd.balance_in_val,
+        bd.balance_in_total,
+        td.turn_deb_rub,
+        td.turn_deb_val,
+        td.turn_deb_total,
+        td.turn_cre_rub,
+        td.turn_cre_val,
+        td.turn_cre_total,
+        bd.balance_out_rub,
+        bd.balance_out_val,
+        bd.balance_out_total
+    FROM balance_data bd
+    LEFT JOIN turnover_data td
+        ON bd.chapter = td.chapter 
+        AND bd.ledger_account = td.ledger_account 
+        AND bd.characteristic = td.characteristic;
+
+	UPDATE "LOGS".ETL_LOG 
+    SET END_TIME = v_end_time_log, duration = v_duration_log, STATUS = 'Success'
+    WHERE PROCESS_NAME = 'fill_f101_round_f' AND LOG_DATE = CURRENT_DATE AND STATUS = 'Start';
+	
+END;
+$BODY$;
+
+
+--Вызов
+CALL "DM".fill_f101_round_f('2018-02-01');
+
+SELECT * FROM "DM".dm_f101_round_f;
---
+++ 4_task/export_import.py
@@ -0,0 +1,133 @@
+import psycopg2
+import csv
+from datetime import datetime
+
+# Настройки подключения к базе данных
+conn = psycopg2.connect(
+    dbname="postgres",
+    user="postgres",
+    password="2002",
+    host="localhost",
+    port="5432"
+)
+
+cursor = conn.cursor()
+
+def log_start(v_start_time_log, process_name):
+    cursor.execute("""
+        INSERT INTO "LOGS".ETL_LOG (PROCESS_NAME, LOG_DATE, START_TIME, END_TIME, STATUS, DURATION)
+        VALUES (%s, CURRENT_DATE, %s, NULL, 'Start', NULL);
+    """, (process_name, v_start_time_log))
+
+# Функция логирования окончания процесса
+def log_end(v_end_time_log, v_duration_log, process_name):
+    cursor.execute("""
+        UPDATE "LOGS".ETL_LOG 
+        SET END_TIME = %s, DURATION = %s, STATUS = 'Success'
+        WHERE PROCESS_NAME = %s 
+          AND LOG_DATE = CURRENT_DATE 
+          AND STATUS = 'Start';
+    """, (v_end_time_log, v_duration_log, process_name))
+
+def export_to_csv():
+
+    cursor.execute('SELECT * FROM "DM".dm_f101_round_f')
+    columns = [desc[0] for desc in cursor.description] # названия колонок из 1 строки
+    rows = cursor.fetchall() # все строки данных
+
+    file_path = r'C:\Users\bokla\OneDrive\Рабочий стол\NeoStudy\Project_Task\4\101.csv'
+
+    process_name = "export_f101"
+    v_start_time_log = datetime.now()
+
+    log_start(v_start_time_log, process_name)
+    # Запись в csv
+    with open(file_path, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(columns)  # заголовки
+        writer.writerows(rows)    # данные
+
+    v_end_time_log = datetime.now()
+    v_duration_log = v_end_time_log - v_start_time_log
+
+    log_end(v_end_time_log, v_duration_log, process_name)
+
+    conn.commit()
+    print(f"Данные успешно выгружены в CSV файл: {file_path}")
+
+def clear_table():
+    cursor.execute('TRUNCATE TABLE "DM".dm_f101_round_f_v2;')
+
+def import_from_csv():
+    file_path = r'C:\Users\bokla\OneDrive\Рабочий стол\NeoStudy\Project_Task\4\101.csv'
+
+    process_name = "import_f101"
+    v_start_time_log = datetime.now()
+
+    log_start(v_start_time_log, process_name)
+
+    with open(file_path, mode='r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        headers = next(reader)  # пропуск заголовка
+
+        for row in reader:
+            processed_row = [
+                (None if value == '' else (0 if value == '0' else value))  # преобразование пустых строк данных
+                for value in row
+            ]
+
+           
+            cursor.execute("""
+                INSERT INTO "DM".dm_f101_round_f_v2 (
+                    from_date, to_date, chapter, ledger_account, characteristic,
+                    balance_in_rub, r_balance_in_rub, balance_in_val, r_balance_in_val,
+                    balance_in_total, r_balance_in_total, turn_deb_rub, r_turn_deb_rub,
+                    turn_deb_val, r_turn_deb_val, turn_deb_total, r_turn_deb_total,
+                    turn_cre_rub, r_turn_cre_rub, turn_cre_val, r_turn_cre_val,
+                    turn_cre_total, r_turn_cre_total, balance_out_rub, r_balance_out_rub,
+                    balance_out_val, r_balance_out_val, balance_out_total, r_balance_out_total
+                ) 
+                VALUES (
+                    %s, %s, %s, %s, %s,
+                    %s, %s, %s, %s, 
+                    %s, %s, %s, %s, 
+                    %s, %s, %s, %s, 
+                    %s, %s, %s, %s, 
+                    %s, %s, %s, %s,
+                    %s, %s, %s, %s
+                )
+            """, processed_row)
+
+    v_end_time_log = datetime.now()
+    v_duration_log = v_end_time_log - v_start_time_log
+
+    log_end(v_end_time_log, v_duration_log, process_name)
+
+    conn.commit()
+    print(f"Данные успешно загружены в таблицу из файла: {file_path}")
+
+
+
+
+
+
+def main():
+    action = input("Выберите действие (1 - экспорт в CSV, 2 - импорт из CSV): ")
+
+    if action == '1':
+        export_to_csv()
+    elif action == '2':
+        clear_table()
+        import_from_csv()
+    else:
+        print("Неверный выбор! Попробуйте снова.")
+
+def close_connection():
+    cursor.close()
+    conn.close()
+
+if __name__ == "__main__":
+    try:
+        main()
+    finally:
+        close_connection()
