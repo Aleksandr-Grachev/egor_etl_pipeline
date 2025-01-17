2238abc7deb9b7bf143f59a99163dcbb87c2083b
Alex Grachev <alex.f.grach@gmail.com>
2025-01-17 13:41:50 +0300

    task2/2 py file fixed csv path

--- 2TASK/2.2_task/import_rd.py
+++ 2TASK/2.2_task/import_rd.py
@@ -18,7 +18,7 @@ cursor = conn.cursor()
 
 
 def import_product():
-    file_path = f"{BASE_CSV_PATH}/2TASK/2.2_task/sources/product_info.csv"
+    file_path = f"{BASE_CSV_PATH}/csv/task2.2/product_info.csv"
 
     with open(file_path, mode='r', encoding='windows-1251') as file:
         reader = csv.reader(file)
@@ -40,7 +40,7 @@ def import_product():
     conn.commit()
 
 def import_deal_info():
-    file_path = f"{BASE_CSV_PATH}/2TASK/2.2_task/sources/deal_info.csv"
+    file_path = f"{BASE_CSV_PATH}/csv/task2.2//deal_info.csv"
 
     with open(file_path, mode='r', encoding='windows-1251') as file:
         reader = csv.reader(file)
