3f111b12c450a49cd9d1f7e5aee3b469526db1a6
егор боклагов <boklagov.egor@yandex.ru>
2025-01-17 14:54:10 +0300

    remark 2.1 task

--- 2TASK/2.1_task/delete_dublicate.sql
+++ 2TASK/2.1_task/delete_dublicate.sql
@@ -1,14 +1,8 @@
---Удаление только дубликатов, без оригинальных записей
 WITH dublicate_rec AS (
-    SELECT 
-        client_rk, 
-        effective_from_date, 
-        ROW_NUMBER() OVER (PARTITION BY client_rk, effective_from_date ORDER BY client_rk) AS dr
-    FROM dm.client
+  SELECT MIN(ctid) AS ctid, client_rk, effective_from_date
+  FROM dm.client
+  GROUP BY client_rk, effective_from_date
 )
-DELETE FROM dm.client
-WHERE (client_rk, effective_from_date) IN (
-    SELECT client_rk, effective_from_date
-    FROM dublicate_rec
-    WHERE dr > 1
-);
+
+DELETE FROM dm.client c
+WHERE ctid NOT IN (SELECT ctid FROM dublicate_rec)
